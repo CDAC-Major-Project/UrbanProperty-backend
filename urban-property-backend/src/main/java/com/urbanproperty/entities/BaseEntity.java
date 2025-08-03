fea3f5f72c408e7cb78333f4a3fa1bbbb45cfec8
kartikey-gour <kartikey.gour.cdac@gmail.com>
2025-08-03 16:15:17 +0530

    added BaseEntity

---
+++ urban-property-backend/src/main/java/com/urbanproperty/entities/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.urbanproperty.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/*
+ * To declare a common base class - w/o any associated table
+ * All other entities will extend n inherit fields
+ */
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+	// add common fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@CreationTimestamp
+	@Column(name="created_time")
+	private LocalDateTime creationDate;
+	@UpdateTimestamp
+	@Column(name="updated_time")
+	private LocalDateTime updatedOn;
+}
