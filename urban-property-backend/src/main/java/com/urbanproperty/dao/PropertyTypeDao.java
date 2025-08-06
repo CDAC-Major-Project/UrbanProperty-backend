7db10b89b62b9b20b5857a505b1ebead709aa10e
kartikey-gour <kartikey.gour.cdac@gmail.com>
2025-08-06 17:58:43 +0530

    updated PropertyTypeServiceImpl, updated PropertyTypeController

--- urban-property-backend/src/main/java/com/urbanproperty/dao/PropertyTypeDao.java
+++ urban-property-backend/src/main/java/com/urbanproperty/dao/PropertyTypeDao.java
@@ -1,10 +1,13 @@
 package com.urbanproperty.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.urbanproperty.entities.PropertyType;
 
 @Repository
 public interface PropertyTypeDao extends JpaRepository<PropertyType, Long> {
-    
+	boolean existsByName(String name);
+	Optional<PropertyType> findByName(String name); 
 }
--- urban-property-backend/src/main/java/com/urbanproperty/dto/ApiResponse.java
+++ urban-property-backend/src/main/java/com/urbanproperty/dto/ApiResponse.java
@@ -1,19 +1,4 @@
 package com.urbanproperty.dto;
-import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ApiResponse {
-	private LocalDateTime timeStamp;
-	private String message;
-
-	public ApiResponse(String message) {
-		this.timeStamp = LocalDateTime.now();
-		this.message = message;
-	}
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
