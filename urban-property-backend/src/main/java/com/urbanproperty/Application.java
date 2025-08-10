e227a27602c7f114793234a3aa668c379619091d
kartikey-gour <kartikey.gour.cdac@gmail.com>
2025-08-10 14:17:21 +0530

    added password encoder bean for password encoding

--- urban-property-backend/src/main/java/com/urbanproperty/Application.java
+++ urban-property-backend/src/main/java/com/urbanproperty/Application.java
@@ -6,6 +6,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
@@ -38,5 +40,10 @@ public class Application {
 
 	}
 	
-
+	//configure PasswordEncoder as spring bean
+			@Bean
+			PasswordEncoder passwordEncoder()
+			{
+				return new BCryptPasswordEncoder();
+			}
 }
